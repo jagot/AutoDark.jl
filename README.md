c6dcb582d397012aa94017db116fae14d7ccbfc6
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2024-06-07 08:26:42 +0200

    React to theme changes, only on macOS for now

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://jagot.github.io/AutoDark.jl/dev/)
 [![Build Status](https://github.com/jagot/AutoDark.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/jagot/AutoDark.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/jagot/AutoDark.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/jagot/AutoDark.jl)
+
+React to theme switches. Only supports macOS for now, using
+[dark-notify](https://github.com/cormacrelf/dark-notify).
+
+![AutoDark.jl demo](./demo.gif)
--- src/AutoDark.jl
+++ src/AutoDark.jl
@@ -1,5 +1,46 @@
 module AutoDark
 
-# Write your package code here.
+DARK_NOTIFY_RUNNING = false
+
+function auto_dark_apple(fun::Function)
+    AutoDark.DARK_NOTIFY_RUNNING && return
+
+    cmd = `dark-notify`
+    pin = Base.PipeEndpoint()
+    pout = Base.PipeEndpoint()
+    perr = Base.PipeEndpoint()
+    proc = run(cmd, pin, pout, perr, wait=false)
+    process_running(proc) || error("Could not start dark-notify")
+    AutoDark.DARK_NOTIFY_RUNNING = true
+    @async try
+        while !eof(pout) && process_running(proc)
+            line = strip(readline(pout))
+            !isempty(line) && fun(Symbol(line))
+        end
+    catch e
+        if e isa ProcessFailedException
+            println("External process failed:")
+            println(e)
+        else
+            rethrow()
+        end
+    finally
+        close(pout)
+        close(pin)
+        close(perr)
+        AutoDark.DARK_NOTIFY_RUNNING = false
+    end
+    nothing
+end
+
+function auto_dark(fun::Function)
+    if Sys.isapple()
+        auto_dark_apple(fun)
+    else
+        error("No support for your OS yet")
+    end
+end
+
+export auto_dark
 
 end
