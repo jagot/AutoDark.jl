54ffa082c9d6d23a4fa80aeed5719d080afe5b8d
Documenter.jl <documenter@juliadocs.github.io>
2024-06-07 06:42:07 +0000

    build based on c6dcb58

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AutoDark","category":"page"},{"location":"#AutoDark","page":"Home","title":"AutoDark","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AutoDark.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AutoDark]","category":"page"}]
+}
